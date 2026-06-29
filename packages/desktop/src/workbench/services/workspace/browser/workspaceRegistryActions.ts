405ead4ce4d7cc749230dd7429ad283247ae41fa
aron-76 <chouarslan@gmail.com>
2026-06-29 15:38:00 +0800

    refactor(workspace): use workbench workspace entry type

--- packages/desktop/src/workbench/services/workspace/browser/workspaceRegistryActions.ts
+++ packages/desktop/src/workbench/services/workspace/browser/workspaceRegistryActions.ts
@@ -1,12 +1,12 @@
 import { nativeHostService } from '../../../../platform/native/browser/nativeHostService.js';
 import { workspaceService } from '../../../../platform/workspaces/browser/workspaceService.js';
-import type { WorkspaceEntry } from '../../../../platform/workspaces/common/workspaces.js';
 import { flushAll } from '../../editor/common/editorFlush.js';
 import { badgeService } from '../../mirror/browser/badgeService.js';
 import { focusService } from '../../mirror/browser/focusService.js';
 import { suspendMirrorWrites } from '../../mirror/browser/mirrorWrites.js';
 import type { WorkspaceRegistryActions } from '../common/workspaceActions.js';
 import { workspaceRefreshPatch } from '../common/workspaceModel.js';
+import type { WorkspaceEntry } from '../common/workspaceTypes.js';
 import { formatWorkspaceError, isWorkspacePathNotFoundError } from './workspaceErrors.js';
 
 const ensuredRoots = new Set<string>();
